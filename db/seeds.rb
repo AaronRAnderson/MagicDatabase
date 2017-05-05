e22c170589592ab3e5c41b5273c62a6533f49a46
Aaron Anderson <aaron.russell.anderson@gmail.com>
2017-05-04 20:12:21 -0600

    Added seed data and made the index page the home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+9.times do |card|
+  Card.create!(
+    name: "Card name: #{card}",
+    converted_manacost: 5,
+      color: "Ruby",
+      description: "Maecenas faucibus mollis interdum.",
+      front_image: "http://placehold.it/280x380",
+      back_image: "https://upload.wikimedia.org/wikipedia/en/a/aa/Magic_the_gathering-card_back.jpg"
+    )
+
+end
+
+
+
+
+
+
+
+
+
+
