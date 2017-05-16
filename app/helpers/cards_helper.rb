3a8bf3b7ccca268b6f5afe036d6fad9208fac7e3
Aaron Anderson <aaron.russell.anderson@gmail.com>
2017-05-15 21:47:43 -0600

    Updated homepage styles

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,12 +1,9 @@
 module CardsHelper
   def default_back_image
-    "https://upload.wikimedia.org/wikipedia/en/a/aa/Magic_the_gathering-card_back.jpg"
+      "https://upload.wikimedia.org/wikipedia/en/a/aa/Magic_the_gathering-card_back.jpg"
   end
 
-
-  # def flip card
-    
-  #   flip_path(card)
-
-  # end
+  def card_size
+    'width: 223, height: 310'
+  end
 end
