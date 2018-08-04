b70541756fab3b641cf3e520ba6911e65b521b17
Aaron Anderson <aaron.russell.anderson@gmail.com>
2018-08-03 21:04:36 -0600

    stuff

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module MagicTheGathering
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -28,5 +27,6 @@ module MagicTheGathering
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
